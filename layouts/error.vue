1c25e1df0376abca0f2e070ff810e644811c359c
Vladimir Slavchev <engineers@infinitylab.world>
2020-07-01 19:36:32 +0700

    Addicted to Danger

---
+++ layouts/error.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="container sohei-error is-fullheight">
+		<h1>Error Page</h1>
+	</div>
+</template>
+<style type="text/css">
+.sohei-error {
+	background-color: red;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+</style>
+<script>	
+export default {
+	props: ['error'],
+	layout: 'default'
+}
+</script>
