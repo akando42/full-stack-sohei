fce77e5e5a268235e902c2805d0370db7ab631e3
Vladimir Slavchev <engineers@infinitylab.world>
2020-07-07 12:27:49 +0700

    Add Sites name

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
   ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Full Stack Sohei - Mastering Full Stack Development',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
